23c3d269da2c5343130c895c82c3ff1e893d0210
JumailaYumna <jumailayumna08@gmail.com>
2026-04-06 11:31:33 +0700

    jum1

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/home', (req, res) => {
+  res.send('Hello dari Express!');
+});
+
+app.listen(port, () => {
+  console.log(`Server jalan di http://localhost:${port}`);
+});
